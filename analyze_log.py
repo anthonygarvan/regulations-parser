86b2da118d28f7b25f50b3366bc5df776a300408
Anthony Garvan <anthony.garvan@gsa.gov>
2016-03-08 11:18:24 -0600

    updates to logging and analysis to include more error types

--- analyze_log.py
+++ analyze_log.py
@@ -1,17 +1,17 @@
 import re
 from collections import Counter
-log = open('all_parses.log')
+log = open('history_run.log')
 from pprint import pprint
 
 c = Counter()
 for line in log:
-	if "Error" in line:
+	if "Error" in line or "Exception" in line:
 		c.update([line])
 
 log.close()
 most_common = c.most_common(15)
 
-results = open('common_errors.csv', 'w')
+results = open('common_errors_history.csv', 'w')
 results.write('Error,Count\n')
 for error, count in most_common:
 	results.write('"%s",%d\n' % (error, count))
